dd724897e54fc281cbd5bdc579a0d706d134721d
nobodycares9393939-ctrl <nobodycares9393939@gmail.com>
2025-12-05 00:24:15 -0600

    Add files via upload

---
+++ scanaware_fixed/README.md
@@ -0,0 +1,3 @@
+ScanAware - Play Store ready scaffold
+Package: commun.scanaware
+Version: 1.0.1
---
+++ scanaware_fixed/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'screens/scan_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(home: ScanScreen()));
+}
