779a030a34d8ccf07c7e002b3098f3b87a12c505
nobodycares9393939-ctrl <nobodycares9393939@gmail.com>
2025-12-05 22:39:06 -0600

    Delete scanaware_fixed_complete/scanaware_final_root directory

--- scanaware_fixed_complete/scanaware_final_root/README.md
+++
@@ -1,2 +0,0 @@
-# ScanAware - Fixed Project
-This project contains fixes for Android v2 embedding and dependency adjustments.
--- scanaware_fixed_complete/scanaware_final_root/lib/services/advanced_lookup.dart
+++
@@ -1,5 +0,0 @@
-class AdvancedLookup {
-  static Future<Map<String,dynamic>> lookup(String barcode) async {
-    return {'barcode': barcode, 'product_name': 'Demo product'};
-  }
-}
